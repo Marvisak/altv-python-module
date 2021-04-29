babd4ba7253a62692d930a21ea716bfbf3bd697c
Daniel Kukucz <kukuczdaniel@gmail.com>
2021-04-30 00:26:18 +0200

    Updated build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
-mkdir build
+IF NOT EXIST build\ (
+    mkdir build
+)
 pushd build
 cmake -G"Visual Studio 16" -DSERVER_MODULE=1 -DPYTHON_EXECUTABLE="../vendors/cpython/lib/python.exe" -A x64 ..
 cmake --build . --config Release
