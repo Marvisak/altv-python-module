1380fffad95a2b1c57be2a065b26c44487db7803
Marvis <60314886+Marvisak@users.noreply.github.com>
2021-05-13 20:51:16 +0200

    Renamed player to base for easier parsing to BaseObject

--- src/classes/player.cpp
+++ src/classes/player.cpp
@@ -2,5 +2,5 @@
 
 void Player::Spawn(float x, float y, float z, unsigned int delay) {
     alt::Position altPos {x, y, z};
-    player->Spawn(altPos, delay);
+    base->Spawn(altPos, delay);
 }
--- src/classes/player.h
+++ src/classes/player.h
@@ -4,13 +4,13 @@
 
 
 class Player {
-    alt::RefBase<alt::RefStore<alt::IPlayer>> player;
+    alt::RefBase<alt::RefStore<alt::IPlayer>> base;
 public:
-    explicit Player(alt::RefBase<alt::RefStore<alt::IPlayer>> &&player) : player(player) {};
+    explicit Player(alt::RefBase<alt::RefStore<alt::IPlayer>> &&player) : base(player) {};
 
     //Health
-    unsigned short GetHealth() const { return player->GetHealth(); }
-    void SetHealth(int number) { player->SetHealth(number); }
+    unsigned short GetHealth() const { return base->GetHealth(); }
+    void SetHealth(int number) { base->SetHealth(number); }
 
     //Position
     void Spawn(float x, float y, float z, unsigned int delay);
