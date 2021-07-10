2026b9c8bde421a2fe9ef11dc0f4238d59032ad7
Zack <zackary12389@yahoo.co.uk>
2021-07-10 20:36:17 +0100

    Add alt getters Added branch, defaultDimension, globalDimension, rootDir, sdkVersion and version getters

--- src/bindings/alt.h
+++ src/bindings/alt.h
@@ -8,5 +8,14 @@ unsigned int Hash(const std::string& strToHash) {
 
 void RegisterHelpersFunctions(py::module_ m)
 {
+    // Getters
+    m.attr("branch") = Core->GetBranch().ToString();
+    m.attr("defaultDimension") = alt::DEFAULT_DIMENSION;
+    m.attr("globalDimension") = alt::GLOBAL_DIMENSION;
+    m.attr("rootDir") = Core->GetRootDirectory().ToString();
+    m.attr("sdkVersion") = Core->SDK_VERSION;
+    m.attr("version") = Core->GetVersion().ToString();
+
+    // Methods
     m.def("hash", &Hash, "Hashes string");
 }
