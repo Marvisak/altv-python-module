8c3cb16f1c46e4a0c0f0ed674f4161dd94dda3a3
Zack <zackary12389@yahoo.co.uk>
2021-08-01 23:21:03 +0100

    Add all other colshape classes
    Added ColshapeCircle, ColshapeCuboid, ColshapeRectangle and ColshapeSphere

--- src/bindings/bindings.h
+++ src/bindings/bindings.h
@@ -24,4 +24,12 @@ PYBIND11_EMBEDDED_MODULE(alt, m)
     RegisterVector3Class(m);
     RegisterRGBAClass(m);
     RegisterVoiceChannelClass(m);
+
+	// Colshapes
+	RegisterColshapeClass(m);
+	RegisterColshapeCircleClass(m);
+	RegisterColshapeCuboidClass(m);
+	RegisterColshapeCylinderClass(m);
+	RegisterColshapeRectangleClass(m);
+	RegisterColshapeSphereClass(m);
 }
--- src/classes/classes.h
+++ src/classes/classes.h
@@ -9,5 +9,10 @@
 #include "base/player.h"
 #include "base/voicechannel.h"
 
+// Colshapes
 #include "base/colshape.h"
+#include "base/colshapes/colshapecircle.h"
+#include "base/colshapes/colshapecuboid.h"
 #include "base/colshapes/colshapecylinder.h"
+#include "base/colshapes/colshaperectangle.h"
+#include "base/colshapes/colshapesphere.h"
