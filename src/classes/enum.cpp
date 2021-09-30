24d38c90c530d3d61061c7f026e513acc0327e96
Zack <zackary12389@yahoo.co.uk>
2021-09-30 22:07:26 +0100

    Add auto enum

--- src/classes/enum.cpp
+++ src/classes/enum.cpp
@@ -12,6 +12,22 @@ py::enum_<E> export_enum(const pybind11::handle &scope, const char* enumName) {
 }
 
 void RegisterEnums(const py::module_& m) {
-    export_enum<RadioStation>(m, "RadioStation");
+	export_enum<ExplosionType>(m, "ExplosionType");
+	export_enum<BodyPart>(m, "BodyPart");
+	export_enum<BlipType>(m, "BlipType");
+	export_enum<ColShapeType>(m, "ColShapeType");
+	export_enum<CheckpointType>(m, "CheckpointType");
+	export_enum<RadioStation>(m, "RadioStation");
+	export_enum<NumberPlateStyle>(m, "NumberPlateStyle");
+	export_enum<VehicleBumper>(m, "VehicleBumper");
+	export_enum<VehicleBumperDamage>(m, "VehicleBumperDamage");
+	export_enum<VehicleDoor>(m, "VehicleDoor");
+	export_enum<VehicleDoorState>(m, "VehicleDoorState");
+	export_enum<VehicleLockState>(m, "VehicleLockState");
+	export_enum<VehicleModType>(m, "VehicleModType");
+	export_enum<VehiclePart>(m, "VehiclePart");
+	export_enum<VehiclePartDamage>(m, "VehiclePartDamage");
+	export_enum<WeatherType>(m, "WeatherType");
+	export_enum<WindowTint>(m, "WindowTint");
     export_enum<alt::IBaseObject::Type>(m, "BaseObjectType");
 }
