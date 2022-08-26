8cda87e093934e2969c26cda34aa5ac1dac033bc
Marvis <kukuczdaniel@gmail.com>
2022-08-26 02:31:58 +0200

    Fixed linux building

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-if [ ! -d "./build" ]; then
+if [ ! -d "./build" ]
+then
     mkdir build
 fi
 
@@ -7,16 +8,20 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 cmake --build . --config Release
 cd ..
 
-if [ ! -d "./dist" ]; then
+if [ ! -d "./dist" ]
+then
     mkdir -p dist/python
 fi
 
 cp build/python-module.so dist
-cp build/Python-3.*.*/libpython3.*.so.1.0 dist
-cp -r build/Python-3.*.*/Lib/* dist/python
-cp -r build/Python-3.*.*/build/lib.linux-x86_64-3.*/* dist/python
-
+cp build/Python-3.*.*/libpython3.*.so.1.0 dist/python
 
 # This is huge absolutely useless directory
-rm -rf dist/python/test
-rm -rf dist/python/__pycache__
+rm -rf build/Python-3.*.*/Lib/test
+rm -rf build/Python-3.*.*/Lib/__pycache__
+
+cd build/Python-3.*.*/Lib/
+zip -rq ../../../dist/python/libs .
+cd ..
+cp -r build/lib.linux-x86_64-3.*/* ../../dist/python
+
